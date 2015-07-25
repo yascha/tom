6a759beb7bd9179158052dfa1b2c009a84a0e560
Yascha Friesen <yfriesen@demonware.net>
2015-07-25 11:55:36 -0700

    Added some brainstorming notes about which classes to create

--- tom/main.cpp
+++ tom/main.cpp
@@ -7,9 +7,39 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string input;
+    
+    cin >> input;
+    
+    cout << "Hello " + input + "!\n";
+    
+    
+    // handle latest input
+    
+    // if we need to make a move, decide on move
+    
+    // communicate our move to the framework
+    
+    
+    
+    
     return 0;
 }
+
+// classes that I need:
+
+// game -- stores the entire game state (points, combo, board, etc)
+
+// board -- subclass of game, stores the game board
+
+// comms -- handles communications from the game engine
+
+// some other stuff related to the specifics of how the actual heuristic turns out (zobrist/hash map)
+
+
+
