140d03b5f4001384e69684eea0e09f98af07ef2f
Yascha Friesen <yfriesen@demonware.net>
2015-07-25 11:22:28 -0700

    Initial Commit

---
+++ tom/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tom
+//
+//  Created by yfriesen on 2015-07-25.
+//  Copyright (c) 2015 tom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
